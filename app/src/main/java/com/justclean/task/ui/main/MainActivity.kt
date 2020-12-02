bcce4ed1c3b5795430977a2c94c268420a3af1f9
Mohamed Ebrahim <ebrahimm131@gmail.com>
2020-12-02 16:10:10 +0400

    add the packages and main and detail activity

--- app/src/main/java/com/justclean/task/ui/main/MainActivity.kt
+++ app/src/main/java/com/justclean/task/ui/main/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.justclean.task
+package com.justclean.task.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.justclean.task.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ app/src/main/java/com/justclean/task/ui/main/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.justclean.task.ui.main
+
+
+/**
+ * Created by @mohamedebrahim96 on 02,December,2020.
+ * ebrahimm131@gmail.com,
+ * Dubai, UAE.
+ */
+class MainViewModel {
+}
