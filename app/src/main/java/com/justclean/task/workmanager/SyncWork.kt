c6e070a556d72ae33cbaac133b66e0727b6ec043
Mohamed Ebrahim <ebrahimm131@gmail.com>
2020-12-04 19:16:26 +0400

    add work manager and testing dependancies

---
+++ app/src/main/java/com/justclean/task/workmanager/SyncWork.kt
@@ -0,0 +1,38 @@
+package com.justclean.task.workmanager
+
+
+/**
+ * Created by @mohamedebrahim96 on 04,December,2020.
+ * ebrahimm131@gmail.com,
+ * Dubai, UAE.
+ */
+import android.content.Context
+import androidx.work.*
+import dagger.hilt.android.qualifiers.ApplicationContext
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+class SyncWork @Inject constructor(
+    @ApplicationContext context: Context
+) {
+    private val workManager = WorkManager.getInstance(context)
+    suspend fun start() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        val workRequest = PeriodicWorkRequestBuilder<SyncWorker>(1, TimeUnit.HOURS)
+            .addTag(SYNC_WORK)
+            .setConstraints(constraints)
+            .build()
+
+        workManager.enqueueUniquePeriodicWork(
+            SYNC_WORK,
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
+        )
+    }
+
+    companion object {
+        private const val SYNC_WORK = "SyncWork"
+    }
+}
